98de82b3094738f234877a16de67b1eb0140a1b7
Martu-t <ma.tarantino@hotmail.com>
2022-12-03 20:08:55 -0300

    upgrade streamlit filters

--- streamlit/app.py
+++ streamlit/app.py
@@ -22,23 +22,26 @@ database_ip= os.getenv('DATABASE_IP')
 database_name=os.getenv('DATABASE_NAME')
 
 
-#def getdata():
-database_conection=db.create_engine(f"postgresql://{database_username}:{database_password}@{database_ip}/{database_name}")
-connection = database_conection.raw_connection()
-metadata=db.MetaData()
-cursor = connection.cursor()
-#df = pd.read_sql_query('SELECT mag, place, time, type, depth, lon, lat FROM public.earthquake ORDER BY time DESC LIMIT 5000;',con=database_conection)
-#df2 = pd.read_sql_query('SELECT mag, place, time, type, depth, lon, lat FROM public.earthquake WHERE ((lat BETWEEN -56.17 AND -16.889) AND (lon BETWEEN -76.553 AND -67.236)) ORDER BY time DESC;',con=database_conection)
+def getdata():
+    database_conection=db.create_engine(f"postgresql://{database_username}:{database_password}@{database_ip}/{database_name}")
+    connection = database_conection.raw_connection()
+    metadata=db.MetaData()
+    cursor = connection.cursor()
+    #df = pd.read_sql_query('SELECT mag, place, time, type, depth, lon, lat FROM public.earthquake ORDER BY time DESC LIMIT 5000;',con=database_conection)
+    #df2 = pd.read_sql_query('SELECT mag, place, time, type, depth, lon, lat FROM public.earthquake WHERE ((lat BETWEEN -56.17 AND -16.889) AND (lon BETWEEN -76.553 AND -67.236)) ORDER BY time DESC;',con=database_conection)
 
-df2 = pd.read_sql_query('SELECT e.place, e.mag, e.time, e.type, e.lat, e.lon, e.depth, t.fecha, t.clasificacion, t.id_ciudad, t.distancia, p.nombre, p.provincia FROM earthquake e JOIN twitter t ON t.id_terremotos = e.id JOIN poblacion_chile p ON  p.id = t.id_ciudad;',con=database_conection)
+    df2 = pd.read_sql_query('SELECT e.place, e.mag, e.time, e.type, e.lat, e.lon, e.depth, t.fecha, t.clasificacion, t.id_ciudad, t.distancia, p.nombre, p.provincia, p.región FROM earthquake e JOIN twitter t ON t.id_terremotos = e.id JOIN poblacion_chile p ON  p.id = t.id_ciudad;',con=database_conection)
 
-connection.close()
-#df = df.dropna()
-#df2 = df2.dropna()
+    connection.close()
+    #df = df.dropna()
+    #df2 = df2.dropna()
 
-chile = df2[df2['place'].str.contains('Chile')]
-chile[['date', 'time_only']] = chile['time'].apply(lambda x: pd.Series(str(x).split(' ')))
-df2[['date', 'time_only']] = df2['time'].apply(lambda x: pd.Series(str(x).split(' ')))
+
+    chile = df2[(df2.place.str.contains('Chile')) | (df2.place.str.contains('Argentina')) | (df2['place'].str.contains('Peru'))]
+    #chile = df2
+    chile[['date', 'time_only']] = chile['time'].apply(lambda x: pd.Series(str(x).split(' ')))
+    #df2[['date', 'time_only']] = df2['time'].apply(lambda x: pd.Series(str(x).split(' ')))
+    return chile 
 
 
     #return chile
@@ -49,7 +52,7 @@ df2[['date', 'time_only']] = df2['time'].apply(lambda x: pd.Series(str(x).split(
 #earthquake = pd.read_csv('/Users/marthatarantino/Desktop/henry/streamlit/transformed1.csv', usecols = fields)
 
 def generate_color(magnitude):
-        if magnitude <= 4:
+        if magnitude <= 4.2:
             c_outline, c_fill = '#4a7a1c', '#4a7a1c'
             m_opacity, f_opacity = 0.9, 0.7
         elif magnitude <= 5:
@@ -64,6 +67,12 @@ def generate_popup(magnitude, depth, time_only):
     return f'''<strong>Magnitude:</strong> {magnitude}<br><strong>Depth:</strong> {depth} km <br><strong>Time:</strong> {time_only}'''    
 
 def main_page():
+    #st.experimental_rerun()
+    chile = getdata()
+    df_sp = chile.drop(['time', 'id_ciudad', 'type', 'fecha'], axis=1)
+    df_sp.columns = ['Localización', 'Magnitud', 'Latitud', 'Longitud', 'Profundidad', 'Clasificación', 'Distancia a comuna chile', 'Comuna', 'Provincia', 'Región', 'Fecha sismo', 'Hora sismo']
+    df_sp = df_sp[['Fecha sismo','Comuna', 'Provincia', 'Región','Magnitud', 'Profundidad', 'Distancia a comuna chile', 'Hora sismo','Clasificación','Localización','Latitud', 'Longitud']]
+
     #getdata()
     st.markdown("Bienvenidos")
     st.subheader('Sistema de alertas sísmico')
@@ -71,13 +80,15 @@ def main_page():
 
     st.title('Datos de los últimos sismos en Chile')
     if st.checkbox('Ver tabla completa'):
-        st.dataframe(chile)
+        st.dataframe(df_sp)
 
-    if st.checkbox('Ver últimos 5 registros'):
-        if st.button('show head'):
-            st.write(chile.head())
-        #if st.button('show tail'):
-            #st.write(chile.tail())
+    if st.checkbox('Ver muestras'):
+        if st.button('Mostrar últimos 5'):
+            st.write(df_sp.head())
+        if st.button('Mostrar primeros 5'):
+            st.write(df_sp.tail())
+        if st.button('Mostrar 1 aleatorio'):
+            st.write(df_sp.sample())    
 
     
     #st.subheader('Tamaño de la tabla')
@@ -102,7 +113,9 @@ def main_page():
     #values = st.slider('Select a range of values',1, 10, (2, 5))
     #st.write('Values:', values)
 
-    mag_max = st.slider('Max Magnitud', 0, 10, 9)
+    #mag_max = st.slider('Max Magnitud', 0, 10, 9)
+    selected_mag_range = st.slider('Selecione rango de magnitud', 0, 10, (1,9))
+    #st.write('selected_mag_range:', selected_mag_range)
     start_date = st.date_input("Start Date", value=pd.to_datetime(chile['date'].iloc[-1], format="%Y-%m-%d"))
     end_date = st.date_input("End Date", value=pd.to_datetime(chile['date'].iloc[1], format="%Y-%m-%d"))
 
@@ -110,18 +123,24 @@ def main_page():
     start_d = start_date.strftime("%Y-%m-%d")
     end_d = end_date.strftime("%Y-%m-%d")
 
+    if st.checkbox('Filtrar por región', value=True):
+        region_list = chile['región'].unique().tolist()
+        region_selected = st.multiselect('Filtrar Regiones', region_list, default=region_list)
+        
     #st.sidebar.slider('Dates', value=datetime(chile['time'].iloc[1]))#, chile['date'].iloc[-1])
-    if st.checkbox('Show Map', value=True):
-
-        #zoom_starts = st.sidebar.slider('Zoom del mapa', 3,15,5)
-        quake_map = folium.Map(
-        location=[-35.675147, -71.542969],
-        zoom_start=5,
-        tiles='Stamen Terrain',
-        width=1024,
-        height=600
-    )
-    dffiltro = chile.loc[((chile['date'] >= start_d) & (chile['date'] <= end_d )) & (chile['mag']<=mag_max)]
+    
+        
+    #zoom_starts = st.sidebar.slider('Zoom del mapa', 3,15,5)
+    quake_map = folium.Map(
+    location=[-35.675147, -71.542969],
+    zoom_start=5,
+    tiles='Stamen Terrain',
+    width=1024,
+    height=600
+)
+    
+    dffiltro = chile.loc[((chile['date'] >= start_d) & (chile['date'] <= end_d )) & ((chile['mag'] >= selected_mag_range[0]) & (chile['mag'] <= selected_mag_range[1]) & (chile['región'].isin(region_selected)) )]
+    #(chile['mag']<=mag_max)]
 
     for _, row in dffiltro.iterrows():
         c_outline, c_fill, m_opacity, f_opacity = generate_color(row['mag'])
@@ -141,8 +160,8 @@ def main_page():
     st.markdown('''
     En el mapa se pueden ver los últimos sismos del país de Chile y alrededores.
     Los círculos están representados con colores, según la magnitud del mismo, siendo:
-    - Verde: Sismos con magnitudes menores a 4
-    - Naranja: Sismos con magnitudes entre 4 y 5
+    - Verde: Sismos con magnitudes menores a 4.2
+    - Naranja: Sismos con magnitudes entre 4.2 y 5
     - Rojo: Sismos con magnitudes mayores a 5
     ''')
 
@@ -157,7 +176,7 @@ def about():
 
     * :information_source: En la sección "I. Sobre nosotros" vas a conocer un poco sobre nuestro trabajo
     
-    * :flag-cl: En la sección de "II. Mapa" vas a ver los últimos registros sobre sismos significativos de Chile. 
+    * :flag-cl: En la sección de "II. Mapa Twitter Chile" vas a ver los últimos registros sobre sismos significativos de Chile. 
 ''')
 
     st.markdown('''
